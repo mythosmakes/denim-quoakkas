dd558e287b67de4d759b39aa824b76ad0e92ed4d
FakexxChaos <mpickersgill@knights.ucf.edu>
2023-02-26 15:37:37 -0500

    menus AGAIN

---
+++ DenimTest/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+
+{
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
